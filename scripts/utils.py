df6a7b96917bf91b40fd83e55ed18caef2c90eec
Andrei Spiridonov <andrey.spiridonov@aisa.ru>
2019-04-09 01:04:10 +0800

    utils scritps

--- scripts/utils.py
+++ scripts/utils.py
@@ -18,7 +18,7 @@ def create_overlays(
         color: Tuple[int, int, int] = (89, 69, 15),
         alpha: float = 0.5,
         verbose: bool = True,
-        ignore_errors: bool = False):
+        ignore_errors: bool = False) -> None:
     # convert all paths to PosixPath
     root_dir = Path(root_dir)
     img_dir = Path(img_dir)
@@ -55,7 +55,7 @@ def create_overlays(
 def make_overlay(
         img: np.ndarray, mask: np.ndarray,
         color: Tuple[int, int, int] = (89, 69, 15),
-        alpha: float = 0.5):
+        alpha: float = 0.5) -> np.ndarray:
     # result img
     output = img.copy()
     # overlay mask
@@ -74,7 +74,7 @@ def make_overlay(
     return output
 
 
-def sort_frame_func(filename: Path):
+def sort_frame_func(filename: Path) -> int:
     matching_numbers = re.findall(r'\\d+', filename.stem)
     num_matches = len(matching_numbers)
     if num_matches == 0:
@@ -84,7 +84,7 @@ def sort_frame_func(filename: Path):
     return int(matching_numbers[0])
 
 
-def make_ordered_directory(directory: Union[str, Path]):
+def make_ordered_directory(directory: Union[str, Path]) -> None:
     res_dir = Path(directory)
     directory = Path(directory)
     millis = int(round(time.time() * 1000))
@@ -102,7 +102,9 @@ def make_ordered_directory(directory: Union[str, Path]):
     shutil.rmtree(str(tmp_dir), ignore_errors=True)
 
 
-def remove_redundant_channels(directory: Union[str, Path]):
+def remove_redundant_channels(
+        directory: Union[str, Path],
+        verbose: bool = True) -> None:
     directory = Path(directory)
     files = directory.iterdir()
     for fn in files:
@@ -113,3 +115,20 @@ def remove_redundant_channels(directory: Union[str, Path]):
             continue
         mask = mask[:, :, 0]
         ret = cv2.imwrite(str(fn), mask)
+        if verbose:
+            print(f'done with {fn.name}')
+
+
+def move_img_to_dir(
+        src_dir: Union[str, Path],
+        dst_dir: Union[str, Path],
+        img_indices: List[int] = None) -> None:
+    src_dir = Path(src_dir)
+    dst_dir = Path(dst_dir)
+    dst_dir.mkdir(parents=True, exist_ok=True)
+    for fn in src_dir.iterdir():
+        if fn.stem.isnumeric():
+            flag = img_indices is None or int(fn.stem) in img_indices
+            if flag:
+                new_fn = dst_dir/fn.name
+                shutil.move(str(fn), str(new_fn))
