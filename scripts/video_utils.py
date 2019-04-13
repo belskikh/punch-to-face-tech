03d38c0e5b7379426fe0482a233ec44836da6bdd
Andrei Spiridonov <andrey.spiridonov@aisa.ru>
2019-04-13 19:26:06 +0800

    ffprobe methods in video_utils

--- scripts/video_utils.py
+++ scripts/video_utils.py
@@ -1,57 +1,133 @@
 import ffmpy
 import subprocess
 import json
+import pickle
 from pathlib import Path
 from typing import Dict, Tuple, List, Union
 
 
-class VideoInfo:
+class VideoFrame:
 
-    def __init__(self, data: Dict) -> None:
-        self.__video_info: Dict = data
+    def __init__(self, frame: Dict) -> None:
+        self.__data = frame
+        self.pkt_pts = int(frame['pkt_pts'])
+        self.pkt_pts_time = float(frame['pkt_pts_time'])
+        self.pkt_duration = int(frame['pkt_duration'])
+        self.pkt_duration_time = float(frame['pkt_duration_time'])
+        self.width = int(frame['width'])
+        self.height = int(frame['height'])
 
-    @classmethod
-    def load_from_file(cls, filename: Union[str, Path]):
-        filename = str(filename)
-        with open(filename) as json_file:
-            data = json.load(json_file)
-        return cls(data)
+    def get_pkt_pts(self) -> int:
+        return self.pkt_pts
 
-    @classmethod
-    def create_from_data(cls, data: Dict):
-        return cls(data)
+    def get_pkt_pts_time(self) -> float:
+        return self.pkt_pts_time
 
-    def __check_data(self):
-        assert self.__video_info is not None, 'No video info data was loaded!'
+    def get_pkt_duration(self) -> int:
+        return self.pkt_duration
 
-    def get_data(self) -> Dict:
-        return self.__video_info
-
-    def get_fps(self) -> float:
-        self.__check_data()
-        fps = eval(self.__video_info['streams'][0]['r_frame_rate'])
-        return fps
+    def get_pkt_duration_time(self) -> float:
+        return self.pkt_duration_time
 
     def get_width(self) -> int:
-        self.__check_data()
-        width = self.__video_info['streams'][0]['width']
-        return int(width)
+        return self.width
 
     def get_height(self) -> int:
-        self.__check_data()
-        height = self.__video_info['streams'][0]['height']
-        return int(height)
+        return self.height
 
-    def get_nb_frames(self) -> int:
-        self.__check_data()
-        num_frames = self.__video_info['streams'][0]['nb_frames']
-        return int(num_frames)
+    def get_data(self) -> Dict:
+        return self.__data
 
-    def save(self, filename: Union[str, Path]) -> None:
-        self.__check_data()
-        filename = str(filename)
-        with open(filename, 'w') as json_file:
-            json.dump(self.__video_info, json_file)
+
+class VideoInfo:
+
+    def __init__(
+            self,
+            name: str,
+            frame_arr: List[Dict],
+            scene_frame_ts_arr: List[Dict[str, int]],
+            video_fps_info: Dict) -> None:
+
+        # name of the video
+        self.name = name
+
+        # save original data
+        self.__frame_arr = frame_arr
+        self.__scene_frame_ts_arr = scene_frame_ts_arr
+        self.__video_fps_info = video_fps_info
+
+        # convert json frames to VideoFrame
+        self.frames = self.__init_frames(self.__frame_arr)
+        # convert json frame timestamps to List[int]
+        self.scene_frame_ts = self.__init_scene_frame_ts(self.__scene_frame_ts_arr)
+
+        # save important video attributes
+        self.width = self.frames[0].get_width()
+        self.height = self.frames[0].get_height()
+        self.num_frames = len(self.frames)
+        self.r_frame_rate = self.__video_fps_info['r_frame_rate']
+        self.avg_frame_rate = self.__video_fps_info['avg_frame_rate']
+        self.time_base = self.__video_fps_info['time_base']
+        self.fps = eval(self.r_frame_rate)
+
+        # get scene pairs (first_frame_in_scene, last_frame_in_scene)
+
+    @staticmethod
+    def __frame_sort_func(frame: VideoFrame) -> int:
+        return frame.get_pkt_pts()
+
+    @staticmethod
+    def __ts_map_func(ts_dict: Dict[str, int]) -> int:
+        return ts_dict['pkt_pts']
+
+    def __init_frames(self, frame_arr: List[Dict]) -> List[VideoFrame]:
+        sort_fn = VideoInfo.__frame_sort_func
+        return sorted(list(map(VideoFrame, frame_arr)), key=sort_fn)
+
+    def __init_scene_frame_ts(
+            self,
+            scene_frame_ts_arr: List[Dict[str, int]]) -> List[int]:
+        return sorted(list(map(VideoInfo.__ts_map_func, scene_frame_ts_arr)))
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_frames(self) -> List[VideoFrame]:
+        return self.frames
+
+    def get_scene_frame_ts(self) -> List[int]:
+        return self.scene_frame_ts
+
+    def get_width(self) -> int:
+        return self.width
+
+    def get_height(self) -> int:
+        return self.height
+
+    def get_num_frames(self) -> int:
+        return self.num_frames
+
+    def get_fps(self) -> float:
+        return self.fps
+
+    def save(self, output_dir: Union[str, Path]) -> None:
+        filename = Path(output_dir)/f'{self.name}.pickle'
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
+
+    @staticmethod
+    def load(filename: Union[str, Path]) -> VideoInfo:
+        video_info = None
+        with open(filename, 'wb') as f:
+            video_info = pickle.load(f)
+        return video_info
+
+
+    # или лучше по номеру кадра их как-то сделать????
+
+
+
+
 
 
 def extract_frames(
@@ -86,20 +162,75 @@ def extract_frames(
 
 def get_video_info(
         filename: Union[str, Path],
+        scene_thresh: float = 0.2,
         verbose: bool = True) -> VideoInfo:
+    frame_arr = get_frames_info(filename)
+    scene_frame_ts_arr = get_scene_frame_ts(filename, scene_thresh)
+    video_fps_info = get_video_fps_info(filename)
+    video_name = Path(filename).stem
+    return VideoInfo(video_name, frame_arr, scene_frame_ts_arr, video_fps_info)
+
+
+def probe_video(
+        filename: Union[str, Path],
+        input_opts: List[str],
+        verbose: bool = True) -> Tuple[bytes, bytes]:
     src_path = str(filename)
-    # input options
-    input_opts = [
-        '-v', 'quiet',
-        '-print_format', 'json',
-        '-select_streams', 'v:0',
-        '-show_streams',
-        '-show_format'
-    ]
+    if not verbose:
+        input_opts.append('-v')
+        input_opts.append('quiet')
     # ffprobe arguments
     inputs = {src_path: input_opts}
     ff = ffmpy.FFprobe(inputs=inputs)
     if verbose:
         print(f'ffprobe cmd: {ff.cmd}')
     stdout, stderr = ff.run(stdout=subprocess.PIPE)
-    return VideoInfo.create_from_data(json.loads(stdout))
+    return stdout, stderr
+
+
+def get_frames_info(
+        filename: Union[str, Path],
+        verbose: bool = True) -> List[Dict]:
+    # input options
+    input_opts = [
+        '-print_format', 'json',
+        '-select_streams', 'v:0',
+        '-show_frames',
+        '-show_entries',
+        'frame=pkt_pts,pkt_pts_time,pkt_duration,pkt_duration_time,width,height'
+    ]
+    stdout, stderr = probe_video(filename, input_opts, verbose)
+    return json.loads(stdout)['frames']
+
+
+def get_scene_frame_ts(
+        filename: Union[str, Path],
+        scene_thresh: float = 0.2,
+        verbose: bool = True) -> List[Dict[str, int]]:
+    src_path = f'movie={filename}, select=gt(scene\\,{scene_thresh})'
+    # input options
+    input_opts = [
+        '-print_format', 'json',
+        '-select_streams', 'v:0',
+        '-f', 'lavfi',
+        '-show_frames',
+        '-show_entries',
+        'frame=pkt_pts'
+    ]
+    stdout, stderr = probe_video(src_path, input_opts, verbose)
+    return json.loads(stdout)['frames']
+
+
+def get_video_fps_info(
+        filename: Union[str, Path],
+        verbose: bool = True) -> Dict:
+    # input options
+    input_opts = [
+        '-print_format', 'json',
+        '-select_streams', 'v:0',
+        '-show_streams',
+        '-show_entries',
+        'stream=r_frame_rate,avg_frame_rate,time_base'
+    ]
+    stdout, stderr = probe_video(filename, input_opts, verbose)
+    return json.loads(stdout)['streams'][0]
