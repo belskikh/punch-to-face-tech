ec6f744bae6f623a148e3c6671063114baa061da
Andrei Spiridonov <andrey.spiridonov@aisa.ru>
2019-04-15 00:09:42 +0800

    fix import bugs

--- scripts/nn/data.py
+++ scripts/nn/data.py
@@ -13,7 +13,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath('..'))
 
-from ..utils import IMG_EXT, num_cpus
+from utils import IMG_EXT, num_cpus
 
 
 class CanvasInferenceDataset(Dataset):
