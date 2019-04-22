5b3d9ea65dbad156b14f91ac7c158589de8c553f
Andrei Spiridonov <andrey.spiridonov@aisa.ru>
2019-04-23 01:14:31 +0800

    script for calc scene homography

--- scripts/canvas_helper.py
+++ scripts/canvas_helper.py
@@ -5,6 +5,7 @@ import cv2
 from pathlib import Path
 from typing import Tuple, List, Union, Dict
 from tqdm import tqdm
+import json
 
 from video_utils import VideoScene
 from calibrate_camera import OctagonMarker
@@ -172,48 +173,6 @@ class HomographyHelper:
         return M
 
 
-def calc_scene_homography(
-        video_scene: VideoScene,
-        frame_dir: Union[str, Path],
-        mask_dir: Union[str, Path]) -> Dict[int, HomographyResult]:
-
-    homo = HomographyHelper()
-
-    prev_frame = None
-    prev_mask = None
-    result = {}
-
-    first_frame = video_scene.get_first_frame()
-    num_frames = video_scene.get_num_frames()
-
-    for frame_n in tqdm(range(first_frame + 1, num_frames)):
-        prev_frame_n = frame_n - 1
-        if prev_frame is None:
-            frame_path = frame_dir / f'{prev_frame_n}.jpg'
-            mask_path = mask_dir / f'{prev_frame_n}.png'
-            prev_frame = cv2.imread(str(frame_path), cv2.IMREAD_COLOR)
-            prev_mask = cv2.imread(str(mask_path), cv2.IMREAD_GRAYSCALE)
-        frame_path = frame_dir / f'{frame_n}.jpg'
-        mask_path = mask_dir / f'{frame_n}.png'
-        frame = cv2.imread(str(frame_path), cv2.IMREAD_COLOR)
-        mask = cv2.imread(str(mask_path), cv2.IMREAD_GRAYSCALE)
-
-        images = (prev_frame, frame)
-        masks = (prev_mask, mask)
-        M = homo.calc_homography(images, masks)
-
-        result[prev_frame_n] = M
-
-        prev_frame = frame
-        prev_mask = mask
-
-    return result
-
-
-# def save_scene_homography():
-#     pass
-
-
 def get_point_pairs_2D(
         frame_points: List[Point],
         texture_points: np.ndarray):
@@ -299,3 +258,253 @@ def calc_homography_and_warp(
         texture_size=texture_size
     )
     return img_warped, mask_warped
+
+
+def calc_scene_homography(
+        init_homo: np.ndarray,
+        video_scene: VideoScene,
+        points0: np.ndarray,
+        point0_ids: List[int],
+        texture_points: np.ndarray,
+        frame_dir: Union[str, Path],
+        mask_dir: Union[str, Path],
+        result_dir: Union[str, Path],
+        texture_dir: Union[str, Path],
+        texture_size: Tuple[int, int] = (1280, 1280),
+        draw_points: bool = False,
+        point_color: Tuple[int, int, int] = (153, 255, 153),
+        point_radius: int = 10,
+        point_font_scale: float = 1.5,
+        point_font_thickness: int = 2
+) -> Dict[int, Dict[str, np.ndarray]]:
+
+    frame_dir = Path(frame_dir)
+    mask_dir = Path(mask_dir)
+
+    # result directories
+    result_dir = Path(result_dir)
+    texture_dir = Path(texture_dir)
+    result_dir.mkdir(parents=True, exist_ok=True)
+    texture_dir.mkdir(parents=True, exist_ok=True)
+
+    result: Dict[int, Dict[str, np.ndarray]] = {}
+
+    homo = HomographyHelper()
+
+    first_frame_n = video_scene.get_first_frame()
+    num_frames = video_scene.get_num_frames()()
+
+    # save init_homo to result
+    prev_frame_n = first_frame_n
+    result[prev_frame_n] = {'texture': init_homo}
+
+    # save previous frame and mask
+    prev_frame, prev_mask = _load_frame_and_mask(
+        frame_n=prev_frame_n,
+        frame_dir=frame_dir,
+        mask_dir=mask_dir
+    )
+
+    # init texture
+    prev_texture, prev_texture_mask = warp_img_to_texture(
+        prev_frame, prev_mask, init_homo,
+        texture_size=texture_size
+    )
+
+    # also save texture with image points
+    if draw_points:
+        img_points = draw_points(
+            prev_texture, texture_points,
+            color=point_color,
+            draw_ids=True,
+            point_ids=list(range(0, 8)),
+            radius=point_radius,
+            font_scale=point_font_scale,
+            font_thickness=point_font_thickness
+        )
+    else:
+        img_points = None
+
+    _save_frame_and_mask(
+        frame=prev_texture,
+        mask=prev_texture_mask,
+        frame_n=prev_frame_n,
+        output_dir=texture_dir,
+        points=img_points
+    )
+
+    # we don't use frame_0
+    # because all information is in init_homo
+    first_frame_n += 1
+
+    for frame_n in tqdm(range(first_frame_n, num_frames)):
+        frame, mask = _load_frame_and_mask(
+            frame_n=frame_n,
+            frame_dir=frame_dir,
+            mask_dir=mask_dir
+        )
+        # считаем homography между предыдущим кадром
+        # в сцене и текущим кадром
+        homo_res = homo.calc_homography(
+            images=(prev_frame, frame),
+            masks=(prev_mask, mask)
+        )
+        if homo_res is None:
+            result[frame_n]['texture'] = None
+            print(f'No homography for frame {frame_n}')
+            continue
+        else:
+            matches, H_frames, status = homo_res
+
+        # считаем временную homography
+        # для преобразования из текущего кадра в текстуру
+        homo_texture = result[prev_frame_n]['texture']
+        H_tmp = homo_texture.dot(np.linalg.inv(H_frames))
+
+        # делаем warp текущего кадра в текстуру
+        tmp_texture, tmp_texture_mask = warp_img_to_texture(
+            frame, mask, H_tmp,
+            texture_size=texture_size
+        )
+
+        # считаем homography между текстурой
+        # текущего кадра и текстурой предыдущего
+        # для корректировки homography
+        homo_res = homo.calc_homography(
+            images=(prev_texture, tmp_texture),
+            masks=(prev_texture_mask, tmp_texture_mask)
+        )
+        if homo_res is None:
+            result[frame_n]['texture'] = None
+            print(f'No homography for frame {frame_n}')
+            continue
+        else:
+            matches, H_tex, status = homo_res
+
+        # H_tmp = texture -> frame
+        # H_tex = texture -> prev_texture
+        # np.linalg.inv(H_tex) = prev_texture -> texture
+
+        # prev_texture -> frame
+        # корректируем homography
+        H_new = np.linalg.inv(H_tex).dot(H_tmp)
+        result[frame_n] = {'texture': H_new}
+
+        # get textures for current frame
+        texture, texture_mask = warp_img_to_texture(
+            frame, mask, H_new,
+            texture_size=texture_size
+        )
+
+        # also save texture with image points
+        if draw_points:
+            img_points = draw_points(
+                texture, texture_points,
+                color=point_color,
+                draw_ids=True,
+                point_ids=list(range(0, 8)),
+                radius=point_radius,
+                font_scale=point_font_scale,
+                font_thickness=point_font_thickness
+            )
+        else:
+            img_points = None
+
+        _save_frame_and_mask(
+            frame=texture,
+            mask=texture_mask,
+            frame_n=frame_n,
+            output_dir=texture_dir,
+            points=img_points
+        )
+
+        # сохраняем данные с текущего кадра
+        prev_frame_n = frame_n
+        prev_frame, prev_mask = frame, mask
+        prev_texture, prev_texture_mask = texture, texture_mask
+
+    # save homographies
+    _save_scene_homo(result)
+    return result
+
+
+def _load_frame_and_mask(
+        frame_n: int,
+        frame_dir: Union[str, Path],
+        mask_dir: Union[str, Path],
+        frame_ext: str = '.jpg',
+        mask_ext: str = '.png'):
+
+    frame_path = frame_dir / f'{frame_n}{frame_ext}'
+    mask_path = mask_dir / f'{frame_n}{mask_ext}'
+
+    frame = cv2.imread(str(frame_path), cv2.IMREAD_COLOR)
+    mask = cv2.imread(str(mask_path), cv2.IMREAD_GRAYSCALE)
+
+    return frame, mask
+
+
+def _save_img(
+        img: np.ndarray,
+        frame_n: int,
+        output_dir: Union[str, Path],
+        img_ext: str = '.jpg'
+) -> None:
+
+    output_dir = Path(output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+
+    img_path = output_dir / f'{frame_n}{img_ext}'
+    ret = cv2.imwrite(str(img_path), img)
+
+
+def _save_frame_and_mask(
+        frame: np.ndarray,
+        mask: np.ndarray,
+        frame_n: int,
+        output_dir: Union[str, Path],
+        points: np.ndarray = None,
+        frame_dir: Union[str, Path] = 'frames',
+        mask_dir: Union[str, Path] = 'masks',
+        point_dir: Union[str, Path] = 'points',
+        frame_ext: str = '.jpg',
+        mask_ext: str = '.png'
+) -> None:
+
+    output_dir = Path(output_dir)
+    frame_dir = output_dir / frame_dir
+    mask_dir = output_dir / mask_dir
+
+    _save_img(frame, frame_n, frame_dir, img_ext=frame_ext)
+    _save_img(mask, frame_n, mask_dir, img_ext=mask_ext)
+
+    if points is not None:
+        point_dir = output_dir / point_dir
+        _save_img(points, frame_n, point_dir, img_ext=frame_ext)
+
+
+def _save_scene_homo(
+        data: Dict,
+        name: str = 'ufc234_gastelum_bisping_1080p_nosound_cut__homography',
+        output_dir: Union[str, Path] = '../data/video/info') -> None:
+    # create output directory
+    output_dir = Path(output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
+    # result filename
+    filename = output_dir / f'{name}.json'
+    # save
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file)
+
+
+def load_scene_homo(
+        name: str = 'ufc234_gastelum_bisping_1080p_nosound_cut__homography',
+        directory: Union[str, Path] = '../data/video/info'
+) -> Dict[int, Dict[str, np.ndarray]]:
+
+        filename = Path(directory) / f'{name}.json'
+
+        data = None
+        with open(filename) as json_file:
+            data = json.load(json_file)
+        return data
